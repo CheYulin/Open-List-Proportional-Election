771897266746e2a3c02d12831a31f0d82fbc23c1
Che Yulin <191836400@qq.com>
2015-11-21 22:31:14 +0800

    add tiny test case

--- src/test/test_main.cpp
+++ src/test/test_main.cpp
@@ -4,7 +4,7 @@
 #include "test_unit.h"
 
 int main() {
-    string in_file_name = "/home/cheyulin/gitrepos/Open-List-Proportional-Representation/input/input_example2_seatnum_1_group1_10_group2_10.txt";
+    string in_file_name = "/home/cheyulin/gitrepos/Open-List-Proportional-Representation/input/input_example1_seatnum_2_group1_3_group2_2.txt";
     IOProcessor my_io_processor;
     Solver *my_solver = my_io_processor.GetSolver(in_file_name);
 
@@ -14,10 +14,10 @@ int main() {
 
     test_unit.TestGroupsInit(my_solver);
 
-    Group to_be_test_group;
-    to_be_test_group.candidates_ ={2,4,5};
-    test_unit.TestFindCertainGroup(to_be_test_group,my_solver->getFirst_party_());
-
+    cout << endl;
+    cout << "First Party";
+    test_unit.TestStrategiesInit(my_solver->getFirst_party_());
+    cout << "Second Party";
     test_unit.TestStrategiesInit(my_solver->getSecond_party_());
 
     delete my_solver;
